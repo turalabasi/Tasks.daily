f285f39143947a81184e52da063435bd9663fdf7
Tamerlan <tamerlanjafar@gmail.com>
2023-08-31 21:42:51 +0400

    Task Exceptions

---
+++ Task Exceptions/src/Task4/Main.java
@@ -0,0 +1,29 @@
+package Task4;
+
+public class Main {
+    public static void main(String[] args) {
+        //Java-da səhvlərin üç əsas kateqoriyası var:
+        //
+        //1.Compile-time errors:: compile xətaları kimi də tanınır, bunlar Java proqramının tərtibi mərhələsində baş verir.
+        // Onlar kodun Java proqramlaşdırma dilinin qaydalarını pozduğunu və bayt koduna yığıla bilməyəcəyini göstərir.
+        // Ümumi nümunələrə sintaksis səhvləri, çatışmayan nöqtəli vergüllər, elan edilməmiş dəyişənlər və ya yanlış metod adından istifadə daxildir.
+        //
+        //Runtime errors: Exception`lar kimi də tanınır, bunlar Java proqramının icrası zamanı baş verir.
+        // İcra zamanı(run time) xətaları proqram işləyərkən baş verən müstəsna şərtləri göstərir.
+        // Onlar yanlış istifadəçi daxiletməsi, yanlış məlumatların işlənməsi və ya resursun əlçatmazlığı kimi müxtəlif amillərdən qaynaqlana bilər.
+        // Ümumi nümunələrə NullPointerException, ArrayIndexOutOfBoundsException və ClassCastException daxildir.
+        //
+        //Məntiqi xətalar: Semantik xətalar kimi də tanınır, bunlar proqram heç bir kompilyasiya və ya icra zamanı xətaları olmadan işlədikdə baş verir,
+        // lakin gözlənilən nəticəni vermir. Məntiqi səhvlər adətən səhv alqoritmlər, qüsurlu proqram məntiqi və ya səhv biznes qaydalarının tətbiqi nəticəsində yaranır.
+        // Bu səhvləri aşkar etmək daha çətindir və tez-tez müəyyən etmək və düzəltmək üçün diqqətli debugging və təhlil tələb edir.
+        //
+        //Java-da error`larin exception`lardan fərqli olduğunu qeyd etmək vacibdir. Səhvlər ümumiyyətlə proqramçının nəzarətindən kənarda olan ciddi problemləri təmsil edir,
+        // məsələn, yaddaşdankənar xətalar (OutOfMemoryError) və ya yığının daşması xətaları (StackOverflowError). Bu səhvlər adətən bərpa olunmur və proqramın dayandırılması ilə nəticələnir.
+        //
+        //İstisnalar, əksinə, proqramçı tərəfindən tutula və idarə oluna bilən müstəsna şərtləri təmsil edir.
+        // Onlar səhvləri zərif şəkildə idarə etməyə və müstəsna hallardan bərpa etməyə imkan verir.
+        //
+        //Java-da müxtəlif növ xətaları başa düşməklə, proqramçılar proqramın hazırlanması və ya icrasının müvafiq mərhələsində problemləri müəyyən edə və həll edə bilərlər.
+    }
+
+}
